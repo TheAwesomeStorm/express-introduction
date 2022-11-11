0865fdd91b798402a0ae811418d7c8a19e62b9f1
Daniel Mendes Sechinske <dms@tokenizadora.com.br>
2022-11-11 16:17:03 -0300

    Added nodemon to watch project changes during development

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const http = require("http");
 
 const port = 3000;
 
-const server = http.createServer((_req, res) => {
+const routes = {
+  '/': 'Built with Node.js',
+  '/book': 'Books display',
+  '/author': 'Author display',
+  '/publisher': 'Publisher display'
+}
+
+const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Node.js');
+  res.end(routes[req.url]);
 });
 
 server.listen(port, () => {
